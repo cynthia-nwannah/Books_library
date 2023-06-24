a0483b00c3eb451600c0274e924a2db517957b3f
Cynthia nwannah <chinwendunwannah@yahoo.com>
2023-06-24 13:31:08 +0100

    update my menu

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-function openMenu() {
-    document.body.classList += " menu--open"
-  }
-  
-  function closeMenu() {
-    document.body.classList.remove('menu--open')
-  }
-  
+
+
+let burger = document.querySelector(".harmburger");
+const nav = document.querySelector(".nav__links");
+const navLink = document.querySelectorAll(".navb");
+
+burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+//    alert("hello")
+navLink.forEach((link, index) => {
+    if (link.style.animation){
+        link.style.animation = "" ;
+    }else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`
+    }
+})
+
+burger.classList.toggle("toggle")
+})
